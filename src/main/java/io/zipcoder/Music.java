22df0efc4206df9e47de827c4fc1e3fe6b85deee
Asia <asia@zipcodersmbp18.mynetworksettings.com>
2024-03-24 22:16:36 -0400

    Finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -5,10 +5,28 @@ public class Music {
     private String[] playList;
 
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+            int counter1 = 0;
+            int counter2 = 0;
+        for( int forwards = startIndex; forwards < playList.length; forwards++) {
+            if(playList[forwards] != selection) {
+                counter1++;
+            } else {
+                break;
+            }
+        }
+        for(int backwards = startIndex; backwards >= 0; backwards--) {
+            if(playList[backwards] != selection) {
+                counter2++;
+            } else {
+                break;
+            }
+
+        }
+        return Math.min(counter1,counter2);
     }
 }
